92f916a8cf2920f17db0805d44f1b96a7816a991
NewLuminous <newluminous7@gmail.com>
2020-11-06 09:49:14 +0700

    Add Word2Vec

--- modeling/logit.py
+++ modeling/logit.py
@@ -1,5 +1,6 @@
 import vectorizing
 import transforming
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 
 class Logit:
@@ -9,15 +10,31 @@ class Logit:
 
     def build_model(self, random_state):
         return LogisticRegression(random_state=random_state)
+        
+    def transform_using_word2vec(self, X):
+        self.vectorizer.fit(X['question'])
+        self.vectorizer.fit(X['answer'])
+        X_question = np.vstack(self.vectorizer.transform(X['question'], minlen=30))
+        X_answer = np.vstack(self.vectorizer.transform(X['answer'], minlen=500))
+        return np.hstack([X_question, X_answer])
 
     def fit(self, X, y):
-        X = transforming.concatenate_after_vectorizing(X, self.vectorizer)
+        if type(self.vectorizer) is vectorizing.Word2Vec:
+            X = self.transform_using_word2vec(X)
+        else:
+            X = transforming.concatenate_after_vectorizing(X, self.vectorizer)
         self.model.fit(X, y)
 
     def predict(self, X):
-        X = transforming.concatenate_after_vectorizing(X, self.vectorizer, do_fit_vectorizer=False)
+        if type(self.vectorizer) is vectorizing.Word2Vec:
+            X = self.transform_using_word2vec(X)
+        else:
+            X = transforming.concatenate_after_vectorizing(X, self.vectorizer, do_fit_vectorizer=False)
         return self.model.predict(X)
 
     def predict_proba(self, X):
-        X = transforming.concatenate_after_vectorizing(X, self.vectorizer, do_fit_vectorizer=False)
+        if type(self.vectorizer) is vectorizing.Word2Vec:
+            X = self.transform_using_word2vec(X)
+        else:
+            X = transforming.concatenate_after_vectorizing(X, self.vectorizer, do_fit_vectorizer=False)
         return self.model.predict_proba(X)
--- vectorizing.py
+++ vectorizing.py
@@ -1,8 +1,44 @@
+import numpy as np
 import tokenizing
 import preprocessing
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+import gensim
 
-VECTORIZERS = ['count', 'tfidf']
+VECTORIZERS = ['count', 'tfidf', 'word2vec']
+
+class Word2Vec:
+    def __init__(self, tokenizer, preprocessor):
+        self.tokenizer = tokenizer
+        self.preprocessor = preprocessor
+        self.model = None
+
+    def fit(self, sentences):
+        if self.model is None:
+            #self.model = gensim.models.Word2Vec(sentences, min_count=10, size=50, workers=12, window=3, sg=1)
+            self.model = gensim.models.Word2Vec.load("saved_models/word2vec_news.model")
+        return self
+
+    def vectorize_sentence(self, sentence, minlen=None):
+        sentence = self.tokenizer(self.preprocessor(sentence))
+        embs = []
+        for token in sentence:
+            if token in self.model.wv:
+                embs.append(self.model.wv[token])
+            else:
+                embs.append(np.zeros(self.model.vector_size))
+
+        embs = np.array(embs)
+        if minlen is not None and embs.shape[0] < minlen:
+            paddings = np.ceil(minlen / embs.shape[0]) - 1
+            d = np.copy(embs)
+            for i in range(int(paddings)):
+                embs = np.concatenate((embs, d))
+            embs = embs[0: minlen]
+
+        return embs.flatten()
+
+    def transform(self, sentences, minlen=None):
+        return sentences.apply(lambda x: self.vectorize_sentence(x, minlen))
 
 def get_vectorizer(vectorizer='tfidf'):
     if vectorizer == 'count':
@@ -11,3 +47,6 @@ def get_vectorizer(vectorizer='tfidf'):
     elif vectorizer == 'tfidf':
         return TfidfVectorizer(tokenizer=tokenizing.tokenize,
                               preprocessor=preprocessing.preprocess)
+    elif vectorizer == 'word2vec':
+        return Word2Vec(tokenizer=tokenizing.tokenize,
+                        preprocessor=preprocessing.preprocess)
