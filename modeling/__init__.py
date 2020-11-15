d23d6aadfb1636b030c829c00beb23a46ca1a537
Nguyen Quoc An <quocanuetcs@gmail.com>
2020-11-15 14:46:58 +0700

    [ADD]:navie bayes

--- modeling/__init__.py
+++ modeling/__init__.py
@@ -2,8 +2,9 @@ from .logit import Logit
 from .logit_embedding import LogitWithEmbedding
 from .crnn import CRNN
 from .crnn_attention import AttentionCRNN
+from .navie_bayes import navieBayes
 
-MODELS = ['logit', 'logit-embedding', 'crnn', 'crnn-attention']
+MODELS = ['logit', 'logit-embedding', 'crnn', 'crnn-attention', 'navie-bayes']
 
 def get_model(model='logit'):
     if model == 'logit':
@@ -14,5 +15,7 @@ def get_model(model='logit'):
         return CRNN
     elif model == 'crnn-attention':
         return AttentionCRNN
+    elif model == 'navie-bayes':
+        return navieBayes
     else:
         raise Exception(f"Model '{model}' not found. Try 'modeling.MODELS' for available models.")
